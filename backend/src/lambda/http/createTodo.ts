4d061ce2f3457b0c1b6805193bd4a030a5862b68
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-05-23 13:40:46 +0100

    feat: create todo
    implemented create todo and store to db
    no auth
    
    footer(ref - optional)

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,45 @@
 import 'source-map-support/register'
+import * as uuid from 'uuid'
+import * as AWS from 'aws-sdk'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult
+} from 'aws-lambda'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const ddbClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
 
   // TODO: Implement creating a new TODO item
-  return undefined
+  const todoId = uuid.v4()
+
+  const todo = {
+    todoId,
+    ...newTodo
+  }
+
+  await ddbClient
+    .put({
+      TableName: todosTable,
+      Item: todo
+    })
+    .promise()
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      todo
+    })
+  }
 }
