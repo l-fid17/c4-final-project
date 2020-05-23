31b7f1d256b68616fcfbdd715e1199b87681cfe5
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-05-23 14:57:36 +0100

    client config

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register'
 
 import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
+// import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
@@ -12,7 +12,7 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = 'https://dev-splam0yx.eu.auth0.com/.well-known/jwks.json'
+// const jwksUrl = 'https://dev-splam0yx.eu.auth0.com/.well-known/jwks.json'
 
 const secret = process.env.AUTH_0_SECRET
 
@@ -59,6 +59,8 @@ export const handler = async (
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
+
+  console.log('src/lambda/auth: ', jwt)
 
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
