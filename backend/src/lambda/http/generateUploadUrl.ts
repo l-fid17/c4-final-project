fa47c54891e2c6ac1b09f64e155b80b9475671ca
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-05-23 14:07:58 +0100

    feat: signed url
    signed url for upload to s3 and update todo item with new img url

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,56 @@
 import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
+import * as uuid from 'uuid'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const ddbClient = new AWS.DynamoDB.DocumentClient()
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const mediaBucket = process.env.S3_BUCKET
+  const expiration = process.env.SIGNED_URL_EXPIRATION
+  const todosTable = process.env.TODOS_TABLE
+
+  const imageId = uuid.v4()
+
+  const s3 = new AWS.S3({ signatureVersion: 'v4' })
+
+  const signedUrl = s3.getSignedUrl('putObject', {
+    Bucket: mediaBucket,
+    Key: imageId,
+    Expires: expiration
+  })
+
+  const imageUrl = `https://${bucket}.s3.amazonaws.com/${imageId}`
+
+  const updatedTodoURL = {
+    TableName: todosTable,
+    Key: { todoId: todoId },
+    UpdateExpression: 'set attachmentUrl = :a',
+    ExpressionAttributeValues: {
+      ':a': imageUrl
+    },
+    ReturnValues: 'UPDATED_NEW'
+  }
+
+  await ddbClient.update(updatedTodoURL).promise()
+  return {
+    statusCode: 202,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      iamgeUrl: imageUrl,
+      uploadUrl: signedUrl
+    })
+  }
 }
